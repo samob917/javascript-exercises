46866e8e22028da99962d4f788a75f68ae9fe58c
Sam Oberly <samob917@outlook.com>
2024-12-28 05:33:51 -0500

    Finish excercise 6 and 7
    Completed all excercises or this specific lesson.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  let degC = (5/9) * (degF-32);
+  return Number(degC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  let degF = ((9/5) * degC) + 32;
+  return Number(degF.toFixed(1));
 };
 
 // Do not edit below this line
